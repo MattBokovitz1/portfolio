783d8e8a5cebcec990cf08ae930791f8cf0715b2
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2021-06-11 11:41:17 -0400

    file structure and completed nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ function Home() {
           <Bio>
             I am a Web Developer in Philadelphia, PA. My current tech stack
             includes React.js and Node.js. I enjoy making websites with clean
-            and navigable designs. <br></br>
+            and navigable designs. I also enjoy building community! <br></br>
             <br></br>
             Also, a large reason for creating this site was to provide a space
             to share my writings. My thoughts on what to write about are derived
